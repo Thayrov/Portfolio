4d52f126f252c3048c762a0f161a881e9ac5cd40
thayrov <thayrovg@gmail.com>
2023-09-05 23:13:47 -0600

    update email address

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:thayrovg@gmail.com">
-          thayrovg@gmail.com
+        <a className="underline" href="mailto:contact@thayrov.com">
+          contact@thayrov.com
         </a>{" "}
         or through this form.
       </p>

b9fa073a0d059ad34cb9fb25c540c2060dfcfd3b
thayrov <thayrovg@gmail.com>
2023-10-12 17:17:52 -0600

    small change in about section

--- components/about.tsx
+++ components/about.tsx
@@ -19,21 +19,21 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-    As an <span className="font-medium">aspiring software developer</span>, I have a solid foundation in 
-    <span className="font-medium"> Node.js, React, Express, and MongoDB</span>. My goal is to deepen my 
-    understanding of <span className="font-medium">software architecture, cloud solutions, and agile methodologies</span>.
-  </p>
-  <p>
-    While I may be at the start of my development career, my previous roles in 
-    <span className="font-medium"> customer service, sales, teaching, management, and designing</span> have equipped me with valuable skills.
-    <span className="italic"> My problem-solving and conflict resolution abilities</span> allow me to navigate challenges in coding projects. 
-    I excel in <span className="underline">team settings</span> due to my strong communication skills. 
-    I also have a keen sense of <span className="font-medium">project management</span>.
-  </p>
-  <p>
-    I bring a <span className="font-medium">mature, well-rounded skill set</span> that complements my technical abilities, 
-    making me a holistic contributor to any team. I'm not just looking for a job; I'm looking to <span className="font-medium">grow, contribute, and excel</span> in a full-time remote position.
-  </p>
+        As an <span className="font-medium">aspiring software developer</span>, I have a solid foundation in 
+        <span className="font-medium"> Node.js, React, Express, and MongoDB</span>. My goal is to deepen my 
+        understanding of <span className="font-medium">software architecture, cloud solutions, and agile methodologies</span>.
+      </p>
+      <p className="mb-3">
+        While I may be at the start of my development career, my previous roles in 
+        <span className="font-medium"> customer service, sales, teaching, management, and designing</span> have equipped me with valuable skills.
+        <span className="italic"> My problem-solving and conflict resolution abilities</span> allow me to navigate challenges in coding projects. 
+        I excel in <span className="underline">team settings</span> due to my strong communication skills. 
+        I also have a keen sense of <span className="font-medium">project management</span>.
+      </p>
+      <p >
+        I bring a <span className="font-medium">mature, well-rounded skill set</span> that complements my technical abilities, 
+        making me a holistic contributor to any team. I'm not just looking for a job; I'm looking to <span className="font-medium">grow, contribute, and excel</span> in a full-time remote position.
+      </p>
     </motion.section>
   );
 }

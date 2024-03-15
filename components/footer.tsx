a53d6dd36b144bffef44661e91dcf63e9327a666
thayrov <thayrovg@gmail.com>
2024-03-14 18:04:10 -0600

    docs: :memo: changes declared license in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,32 +1,35 @@
-"use client";
+'use client';
 
-import React from "react";
-import { useLang } from "@/context/lang-context";
+import React from 'react';
+import {useLang} from '@/context/lang-context';
 
 export default function Footer() {
-  const { lang } = useLang()
+  const {lang} = useLang();
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
-      { lang === 'en' ?  (<>
-      <small className="mb-2 block text-xs">
-        &copy; 2023 Thayrov. All rights reserved.
-      </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
-      </p>
-      </>) : (<>
-        <small className="mb-2 block text-xs">
-        &copy; 2023 Thayrov. Todos los derechos reservados.
-      </small>
-      <p className="text-xs">
-        <span className="font-semibold">Acerca de este sitio web:</span> construido con
-        React y Next.js (App Router y Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email y Resend, hosting en Vercel.
-      </p>
-
-      </>) }
+    <footer className='mb-10 px-4 text-center text-gray-500'>
+      {lang === 'en' ? (
+        <>
+          <small className='mb-2 block text-xs'>
+            &copy; 2023 Thayrov. Released under the MIT License..
+          </small>
+          <p className='text-xs'>
+            <span className='font-semibold'>About this website:</span> built with React & Next.js
+            (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email &
+            Resend, Vercel hosting.
+          </p>
+        </>
+      ) : (
+        <>
+          <small className='mb-2 block text-xs'>
+            &copy; 2023 Thayrov. Liberado bajo la Licencia MIT.
+          </small>
+          <p className='text-xs'>
+            <span className='font-semibold'>Acerca de este sitio web:</span> construido con React y
+            Next.js (App Router y Server Actions), TypeScript, Tailwind CSS, Framer Motion, React
+            Email y Resend, hosting en Vercel.
+          </p>
+        </>
+      )}
     </footer>
   );
 }

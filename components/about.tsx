65ea3864e42021051a277c6fc0b8b372b28c73af
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2024-11-20 00:45:49 -0600

    refactor: improve wording and structure in About component for clarity and conciseness

--- components/about.tsx
+++ components/about.tsx
@@ -22,80 +22,77 @@ export default function About() {
         <>
           <SectionHeading>About me</SectionHeading>
           <p className='mb-3'>
-            I have experience in
+            Experienced in
             <span className='font-medium'>
               {' '}
               Typescript, Node JS, React, React Native, Redux Toolkit, Next JS, Express, PostgreSQL,
-              MongoDB, GraphQL and Apollo Client
+              MongoDB, GraphQL, and Apollo Client
             </span>
-            . My goal is to deepen my understanding of{' '}
+            . Striving to deepen understanding of{' '}
             <span className='font-medium'>
               software architecture, cloud solutions, and agile methodologies
             </span>
             .
           </p>
           <p className='mb-3'>
-            My previous roles in
+            Previous roles in
             <span className='font-medium'>
               {' '}
               customer service, sales, teaching, management, and designing
             </span>{' '}
-            have equipped me with valuable skills.
-            <span className='italic'>
-              {' '}
-              My problem-solving and conflict resolution abilities
-            </span>{' '}
-            allow me to navigate challenges in coding projects. I excel in{' '}
-            <span className='underline'>team settings</span> due to my strong communication skills.
-            I also have a keen sense of <span className='font-medium'>project management</span>.
+            have equipped with valuable skills.
+            <span className='italic'> Problem-solving and conflict resolution abilities</span> allow
+            navigation of challenges in coding projects. Excel in{' '}
+            <span className='underline'>team settings</span> due to strong communication skills.
+            Possess a keen sense of <span className='font-medium'>project management</span>.
           </p>
           <p>
-            I bring a <span className='font-medium'>mature, well-rounded skill set</span> that
-            complements my technical abilities, making me a holistic contributor to any team. I'm
-            not just looking for a job; I'm looking to{' '}
-            <span className='font-medium'>grow, contribute, and excel</span> in a full-time remote
-            position.
+            Bring a <span className='font-medium'>mature, well-rounded skill set</span> that
+            complements technical abilities, making a holistic contributor to any team. Seeking to{' '}
+            <span className='font-medium'>grow, contribute, and excel</span> in full-time remote
+            positions.
           </p>
         </>
       ) : (
         <>
           <SectionHeading>Sobre mí</SectionHeading>
           <p className='mb-3'>
-            Tengo experiencia en
+            Con experiencia en
             <span className='font-medium'>
               {' '}
               Typescript, Node JS, React, React Native, Redux Toolkit, Next JS, Express, PostgreSQL,
               MongoDB, GraphQL y Apollo Client
             </span>
-            . Mi objetivo es profundizar mi comprensión en{' '}
+            . Con miras a profundizar comprensión en{' '}
             <span className='font-medium'>
               arquitectura de software, soluciones en la nube y metodologías ágiles
             </span>
             .
           </p>
           <p className='mb-3'>
-            Mis roles anteriores en
+            Roles anteriores en
             <span className='font-medium'>
               {' '}
               atención al cliente, ventas, enseñanza, gestión y diseño
             </span>{' '}
-            me han equipado con habilidades valiosas.
+            han proporcionado habilidades valiosas.
             <span className='italic'>
               {' '}
-              Mi habilidad para resolver problemas y gestionar conflictos
+              Habilidad para resolver problemas y gestionar conflictos
             </span>{' '}
-            me permite destacar en <span className='underline'>
+            que permite destacar en <span className='underline'>
               entornos de trabajo en equipo
             </span>{' '}
-            gracias a mis fuertes habilidades de comunicación, y buena visión para la
+            gracias a fuertes habilidades de comunicación y buena visión para la
             <span className='font-medium'> gestión de proyectos</span>.
           </p>
           <p>
-            Aporto un <span className='font-medium'>conjunto de habilidades maduro y completo</span>{' '}
-            que complementa mis habilidades técnicas, convirtiéndome en un contribuyente integral
-            para cualquier equipo. No estoy buscando simplemente un trabajo; busco{' '}
-            <span className='font-medium'>crecer, contribuir y sobresalir</span> en una posición
-            remota a tiempo completo.
+            Se aporta un{' '}
+            <span className='font-medium'>conjunto de habilidades maduro y completo</span> que
+            complementa habilidades técnicas, convirtiéndose en un contribuyente integral para
+            cualquier equipo. Buscando{' '}
+            <span className='font-medium'>crecer, contribuir y sobresalir</span> en posiciones
+            remotas a tiempo completo.
           </p>
         </>
       )}

7a06b26f35081aeef27b7cb5d945dc8baa37896e
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2024-11-20 00:44:21 -0600

    refactor: update threshold logic in useSectionInView hook for improved readability

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,15 +1,15 @@
-import type { SectionName } from "./types";
-import { useActiveSectionContext } from "@/context/active-section-context";
-import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
+import type {SectionName} from './types';
+import {useActiveSectionContext} from '@/context/active-section-context';
+import {useEffect} from 'react';
+import {useInView} from 'react-intersection-observer';
 
-export function useSectionInView(sectionName: SectionName, defaultThreshold = 0.75) {
-  const threshold = sectionName === "Experience" ? 0.35 : "Projects" ? 0.15 : defaultThreshold;
+export function useSectionInView(sectionName: SectionName, defaultThreshold = 0.2) {
+  const threshold = defaultThreshold; // sectionName === 'Experience' ? 0.35 : sectionName === 'Projects' ? 0.15 : defaultThreshold;
 
-  const { ref, inView } = useInView({
+  const {ref, inView} = useInView({
     threshold,
   });
-  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+  const {setActiveSection, timeOfLastClick} = useActiveSectionContext();
 
   useEffect(() => {
     if (inView && Date.now() - timeOfLastClick > 1000) {

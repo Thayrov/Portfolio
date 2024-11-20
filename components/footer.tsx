2dc4bd4f445529513d73efe93898a79fb2e1c97f
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2024-11-20 00:46:06 -0600

    refactor: enhance accessibility and improve wording in Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,27 +6,22 @@ import {useLang} from '@/context/lang-context';
 export default function Footer() {
   const {lang} = useLang();
   return (
-    <footer className='mb-10 px-4 text-center text-gray-500'>
+    <footer className='mb-10 px-4 text-center text-gray-500' aria-label='Footer' role='contentinfo'>
       {lang === 'en' ? (
         <>
-          <small className='mb-2 block text-xs'>
-            &copy; 2023 Thayrov. Released under the MIT License..
-          </small>
+          <small className='mb-2 block text-xs'>Released under the MIT License</small>
           <p className='text-xs'>
-            <span className='font-semibold'>About this website:</span> built with React & Next.js
-            (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email &
-            Resend, Vercel hosting.
+            <strong>About this website:</strong> built with React & Next.js (App Router & Server
+            Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
           </p>
         </>
       ) : (
         <>
-          <small className='mb-2 block text-xs'>
-            &copy; 2023 Thayrov. Liberado bajo la Licencia MIT.
-          </small>
+          <small className='mb-2 block text-xs'>Liberado bajo la Licencia MIT.</small>
           <p className='text-xs'>
-            <span className='font-semibold'>Acerca de este sitio web:</span> construido con React y
-            Next.js (App Router y Server Actions), TypeScript, Tailwind CSS, Framer Motion, React
-            Email y Resend, hosting en Vercel.
+            <strong>Acerca de este sitio web:</strong> construido con React y Next.js (App Router y
+            Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email y Resend, hosting
+            en Vercel.
           </p>
         </>
       )}

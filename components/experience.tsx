536911b320afbded2116494b200e943f9a711058
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2024-11-26 21:43:17 -0600

    refactor: adjust padding and improve spacing in Experience component

--- components/experience.tsx
+++ components/experience.tsx
@@ -28,7 +28,7 @@ export default function Experience() {
                 boxShadow: 'none',
                 border: '1px solid rgba(0, 0, 0, 0.05)',
                 textAlign: 'left',
-                padding: '1.3rem 2rem',
+                padding: '1.5rem',
               }}
               contentArrowStyle={{
                 borderRight:
@@ -37,16 +37,17 @@ export default function Experience() {
                     : '0.4rem solid rgba(255, 255, 255, 0.5)',
               }}
               date={item.date}
+              dateClassName='min-[1170px]:!px-2'
               icon={item.icon}
               iconStyle={{
                 background: theme === 'light' ? 'white' : 'rgba(255, 255, 255, 0.15)',
                 fontSize: '1.5rem',
               }}>
               <article>
-                <h3 className='font-semibold capitalize'>
+                <h3 className='font-semibold capitalize pb-1'>
                   {lang === 'en' ? item.title : item.title_es}
                 </h3>
-                <p className='font-normal !mt-0'>
+                <p className='!text-sm !font-light !mt-0 pb-4'>
                   {lang === 'en' ? item.location : item.location_es}
                 </p>
                 <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>
